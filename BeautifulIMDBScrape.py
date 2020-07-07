09a607fb3b0ccedbdcfe965e1ed12f9526f20a5c
T1geress <tavitragus@gmail.com>
2020-07-07 16:47:48 -0500

    code very close complete, but not working 7/7

--- BeautifulIMDBScrape.py
+++ BeautifulIMDBScrape.py
@@ -4,10 +4,10 @@ from bs4 import BeautifulSoup
 
 url = 'https://www.imdb.com/search/title/?genres=adventure&sort=user_rating,desc&title_type=tv_series,mini_series&num_votes=5000,&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=f85d9bf4-1542-48d1-a7f9-48ac82dd85e7&pf_rd_r=NZR78MD1KBAVXZPG5PKK&pf_rd_s=right-6&pf_rd_t=15506&pf_rd_i=toptv&ref_=chttvtp_gnr_2'
 csvfile = open("top50animated.csv", "w")
-csv_writer = csv.writer(csvfile)
-csv_writer.writerow(['rank', 'name', 'length', 'genre', 'rating'])
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
+csv_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+csv_writer.writerow(['rank', 'name', 'length', 'genre', 'rating'])
 results = soup.find("div", class_ = "lister-list")
 movies = results.find_all("div", class_ = "lister-item")
 for movie in movies:
@@ -26,3 +26,4 @@ for movie in movies:
   genre = genre_elem.text.strip()
   rating = rating_elem.text.strip()
   csv_writer.writerow([rank, name, length, genre, rating])
+  print(rank, name, length, genre, rating)
